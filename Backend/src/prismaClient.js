896ca40c929af887ef27054b315885e3056bd9d8
rostiSe <150136554+rostiSe@users.noreply.github.com>
2025-03-24 22:21:38 +0100

    Trying to make auth work in prod - Simplified Prisma client init

--- Backend/src/prismaClient.js
+++ Backend/src/prismaClient.js
@@ -2,10 +2,6 @@
 import { PrismaClient } from '@prisma/client';
 
 
-const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma;
-}
+const prisma = new PrismaClient();
 
 export default prisma;
