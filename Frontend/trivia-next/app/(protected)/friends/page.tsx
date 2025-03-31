f981ced6316fa60939d91e7d55ac389fdcc4aafc
rostiSe <150136554+rostiSe@users.noreply.github.com>
2025-03-31 12:27:33 +0200

    fixing friends list

--- Frontend/trivia-next/app/(protected)/friends/page.tsx
+++ Frontend/trivia-next/app/(protected)/friends/page.tsx
@@ -3,7 +3,14 @@ import FriendsSearch from './FriendsSearch'
 
 export default async function FriendsPage() {
         const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users`)
+        if (!response.ok) {
+            throw new Error('Failed to fetch users')
+        }
+
         const users = await response.json()
+        if (!users) {
+            throw new Error('No users found')
+        }
         console.log(users)
   return (
     <div>
